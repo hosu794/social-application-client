962a06bb193df9395d6f9f28cb279d0f057096af
Grzegorz Szczesny <grzeszczesny14@gmail.com>
2020-08-10 19:47:13 +0200

    Finish comment.actions.js, propably with bugs to fix.

--- src/_actions/comment.actions.js
+++ src/_actions/comment.actions.js
@@ -2,8 +2,19 @@ import { commentConstants, authConstants } from "../_constants";
 import { commentService } from "../_services";
 import { alertActions } from "./";
 import { history, handleResponse } from "../_helpers";
+import { user } from "../_reducers/user.reducer";
+import Axios from "axios";
 
-export const commentActions = { getAllComments };
+export const commentActions = {
+  getAllComments,
+  getCommentById,
+  createComment,
+  getCommentsByCreatedBy,
+  deleteComment,
+  getCommentsByUserId,
+  getCommentsByStoryId,
+  updateComment,
+};
 
 function getAllComments(page, service = commentService.getAllComments) {
   return (dispatch) => {
@@ -70,6 +81,8 @@ function createComment(commentRequest, service = commentService.createComment) {
     return service(commentRequest).then(
       (response) => {
         dispatch(success(response.data));
+        window.location.reload(true);
+        dispatch(alertActions.success("Comment created successfully"));
       },
       (error) => {
         handleResponse(error);
@@ -95,4 +108,179 @@ function createComment(commentRequest, service = commentService.createComment) {
 function getCommentsByCreatedBy(
   username,
   service = commentService.getCommentsByCreatedBy
-) {}
+) {
+  return (dispatch) => {
+    dispatch(request(username));
+
+    return service(username).then(
+      (response) => {
+        dispatch(success(response.data));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
+
+  function request(username) {
+    return {
+      type: commentConstants.GET_COMMENTS_BY_CREATED_BY_REQUEST,
+      username,
+    };
+
+    function success(response) {
+      return {
+        type: commentConstants.GET_COMMENTS_BY_CREATED_BY_SUCCESS,
+        response,
+      };
+    }
+
+    function failure(error) {
+      return {
+        type: commentConstants.GET_COMMENTS_BY_CREATED_BY_FAILURE,
+        error,
+      };
+    }
+  }
+}
+
+function deleteComment(commentId, service = commentService.deleteComment) {
+  return (dispatch) => {
+    dispatch(request(commentId));
+
+    return service(commentId).then(
+      (response) => {
+        dispatch(success(response.data));
+        dispatch(alertActions.success("Comment deleted successfully"));
+        window.location.reload(true);
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
+
+  function request(commentId) {
+    return {
+      type: commentConstants.DELETE_COMMENT_REQUEST,
+      commentId,
+    };
+  }
+
+  function success(response) {
+    return { type: commentConstants.DELETE_COMMENT_SUCCESS, response };
+  }
+
+  function failure(error) {
+    return { type: commentConstants.DELETE_COMMENT_FAILURE, error };
+  }
+}
+
+function getCommentsByUserId(
+  userId,
+  service = commentService.getCommentsByUserId
+) {
+  return (dispatch) => {
+    dispatch(request(userId));
+
+    return service(userId).then(
+      (response) => {
+        dispatch(success(response.data));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
+
+  function request(userId) {
+    return { type: commentConstants.GET_COMMENTS_BY_USER_ID_REQUEST, userId };
+  }
+
+  function success(response) {
+    return { type: commentConstants.GET_COMMENTS_BY_USER_ID_SUCCESS, response };
+  }
+
+  function failure(error) {
+    return { type: commentConstants.GET_COMMENTS_BY_STORY_ID_FAILURE, error };
+  }
+}
+
+function getCommentsByStoryId(
+  storyId,
+  service = commentService.getCommentsByStoryId
+) {
+  return (dispatch) => {
+    dispatch(request(storyId));
+
+    return service(storyId).then(
+      (response) => {
+        dispatch(success(response.data));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
+
+  function request(storyId) {
+    return { type: commentConstants.GET_COMMENTS_BY_STORY_ID_REQUEST, storyId };
+  }
+
+  function success(response) {
+    return {
+      type: commentConstants.GET_COMMENTS_BY_STORY_ID_SUCCESS,
+      response,
+    };
+  }
+
+  function failure(error) {
+    return { type: commentConstants.GET_COMMENTS_BY_STORY_ID_FAILURE, error };
+  }
+}
+
+function updateComment(
+  commentRequest,
+  commentId,
+  service = commentService.updateComment
+) {
+  return (dispatch) => {
+    dispatch(request(commentRequest, commentId));
+
+    return service(commentRequest, commentId).then(
+      (response) => {
+        dispatch(success(response.data));
+        dispatch(alertActions.success("Comment updated successfully"));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
+
+  function request(commentRequest, commentId) {
+    return {
+      type: commentConstants.UPDATE_COMMENT_REQUEST,
+      commentRequest,
+      commentId,
+    };
+  }
+
+  function success(response) {
+    return { type: commentConstants.UPDATE_COMMENT_SUCCESS, response };
+  }
+
+  function failure(error) {
+    return { type: commentConstants.UPDATE_COMMENT_FAILURE, error };
+  }
+}
